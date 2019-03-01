3afc46b11c120fcb0c0ec2a78ae4ad2878b83f2f
Vyacheslav Biryukov <networmx128bit@gmail.com>
2019-03-01 12:29:35 +0300

    Initial Commit

---
+++ dvd-screensaver/dvd_screensaverView.h
@@ -0,0 +1,13 @@
+//
+//  dvd_screensaverView.h
+//  dvd-screensaver
+//
+//  Created by Вячеслав Бирюков on 01.03.19.
+//  Copyright © 2019 Вячеслав Бирюков. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface dvd_screensaverView : ScreenSaverView
+
+@end
